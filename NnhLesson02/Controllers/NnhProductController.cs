91194fe78e95bdcbefdfa3579ce1d7497a8f3bd9
NgocHien205 <nguyenhien13062005@gmail.com>
2025-05-17 15:08:09 +0700

    add lesson02

---
+++ NnhLesson02/Controllers/NnhProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NnhLesson02.Controllers
+{
+    public class NnhProductController : Controller
+    {
+        public IActionResult NnhIndex()
+        {
+            ViewData["messageVD"] = "Du lieu tu viewData";
+            ViewBag.messageVB = "Du lieu tu viewBag";
+            TempData["messageTD"] = "Du lieu tu TempData";
+            return View();
+        }
+        
+       
+    }
+}
---
+++ NnhLesson02/Models/NnhProduct.cs
@@ -0,0 +1,10 @@
+﻿namespace NnhLesson02.Models
+{
+    public class NnhProduct
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int YearRelease { get; set; }
+        public double Price { get; set; }
+    }
+}
