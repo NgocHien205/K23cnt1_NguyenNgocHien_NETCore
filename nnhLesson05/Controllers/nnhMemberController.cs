5d8da0014661184e2cd9536b0f157a7a9238e5ed
NgocHien205 <nguyenhien13062005@gmail.com>
2025-05-24 23:32:09 +0700

    add lesson05

---
+++ nnhLesson05/Controllers/nnhMemberController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using nnhLesson05.Models.DataModels;
+
+
+namespace nnhLesson05.Controllers
+{
+    public class nnhMemberController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult nnhGetMember()
+        {
+            var NnhMember = new nnhMember();
+            NnhMember.nnhMemberId = Guid.NewGuid().ToString();
+            NnhMember.nnhUserName = "Ngọc Hiến";
+            NnhMember.nnhFullName = "Nguyễn Ngọc Hiến";
+            NnhMember.nnhPassword = "1234";
+            NnhMember.nnhEmail = "ngochien@gmail.com";
+            
+            ViewBag.NnhMember = NnhMember;
+            return View(NnhMember);
+        }
+
+        public IActionResult nnhGetMembers()
+        {
+            List<nnhMember> nnhMembers = new List<nnhMember>()
+    {
+        new nnhMember{nnhMemberId = Guid.NewGuid().ToString(), nnhUserName = "Hiến", nnhFullName = "Nguyễn Ngọc Hiến", nnhPassword = "1234", nnhEmail = "ngochien@gmail.com"},
+        new nnhMember{nnhMemberId = Guid.NewGuid().ToString(), nnhUserName = "member2", nnhFullName = "Thành viên 2", nnhPassword = "123456", nnhEmail = "tv2@gmail.com"},
+        new nnhMember{nnhMemberId = Guid.NewGuid().ToString(), nnhUserName = "member3", nnhFullName = "Thành viên 3", nnhPassword = "123456", nnhEmail = "tv3@gmail.com"},
+        new nnhMember{nnhMemberId = Guid.NewGuid().ToString(), nnhUserName = "member4", nnhFullName = "Thành viên 4", nnhPassword = "123456", nnhEmail = "tv4@gmail.com"},
+        new nnhMember{nnhMemberId = Guid.NewGuid().ToString(), nnhUserName = "member5", nnhFullName = "Thành viên 5", nnhPassword = "123456", nnhEmail = "tv5@gmail.com"},
+    };
+            ViewBag.nnhMembers = nnhMembers;
+            return View(nnhMembers);
+        }
+        public IActionResult CreateGetMembers()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ nnhLesson05/Models/DataModels/nnhMember.cs
@@ -0,0 +1,11 @@
+﻿namespace nnhLesson05.Models.DataModels
+{
+    public class nnhMember
+    {
+        public string nnhMemberId { get; set; }
+        public string nnhUserName { get; set; }
+        public string nnhFullName { get; set; }
+        public string nnhPassword { get; set; }
+        public string nnhEmail { get; set; }
+    }
+}
